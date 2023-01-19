76b44fe4cb022489e46d53c3749f8298c25cacb1
Jose Nieto <jose.nieto@inglesparatodos.edu.co>
2023-01-19 14:01:09 -0500

    sum rest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title: string = 'Contador App';
+  contador: number = 0;
+
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  sumar(){
+    this.contador++;
+  }
+  restar(){
+    this.contador--;
+  }
 }
+
+
