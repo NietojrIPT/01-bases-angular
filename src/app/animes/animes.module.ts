ec6e5b98e6839c33f2e8de87d4d6ceb1d133326d
Jose Nieto <jose.nieto@inglesparatodos.edu.co>
2023-01-23 12:48:16 -0500

    minor changes

---
+++ src/app/animes/animes.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AnimeComponent } from './anime/anime.component';
+import { ListadoComponent } from './listado/listado.component';
+ 
+@NgModule({
+  declarations: [
+    AnimeComponent,
+    ListadoComponent
+  ],
+  imports: [
+    CommonModule,
+    
+  ],
+  exports: [
+    ListadoComponent
+  ]
+})
+export class AnimesModule { }
